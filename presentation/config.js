e64007ae5f4f6d7ed1bdb3b66ada4bc0946ab1c1
sebh96 <sebastian.hocke96@gmail.com>
2026-04-13 10:51:05 +0000

    add bim, living room and presentation

---
+++ presentation/config.js
@@ -0,0 +1,6 @@
+// Configuration for the presentation
+window.PRESENTATION_CONFIG = {
+    chatUrl: 'http://localhost:3000/assistant',
+    virtualRoomUrl: 'http://localhost:8085',
+    bimViewerUrl: 'http://localhost:8001/view'
+};
